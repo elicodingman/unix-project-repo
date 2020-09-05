1dd8cae79da327da97f4a1dda2c22dda932cea81
Human <elicoding@protonmail.com>
2020-09-05 18:29:59 -0400

    Wrote the code for guessinggame.sh, I hope I did it right lol.

---
+++ guessinggame.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+## should know how many files are in a directory
+## should present an error if the person doesn't guess right
+## should stop if the person does guess right
+
+count_files() {
+num_of_files=$((ls -A | wc -l))
+}
+
+ask_for_input() {
+echo "Hello player! Let's test your guessing skills! How many files do you think are in the current directory?"
+read response
+}
+
+input_isnum() {
+integers='^[0-9]+$'
+if ! [[ $response=$integers ]] ; then
+  echo "I'm only good at reading integers. You understand, right? Please enter an integer as your guess."
+elif [[ $response=$integers.$integers ]] ; then
+  echo "Did you know that you can't have a fraction of a file in a directory? Wild! Please, enter a whole number this time."
+else
+  output=$True
+}
+
+main() {
+while [[ $output=$True ]] ;
+  if [[ $response -lt $num_of_files ]] ; then
+  echo "You guessed too low! Try again, better luck next time!"
+  if [[ $response -gt $num_of_files ]] ; then
+    echo "You guessed too high! Try again, better luck next time!"
+  if [[ $response -eq $num_of_files ]] ; then
+    echo "How'd you guess? That's right! Let's play again some time."
+}
