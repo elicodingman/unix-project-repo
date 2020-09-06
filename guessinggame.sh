d3180324681b436f6328b581d7a0ab669c094086
Human <elicoding@protonmail.com>
2020-09-06 13:51:49 -0400

    It's still wrong but I changed it

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
-## should know how many files are in a directory
-## should present an error if the person doesn't guess right
-## should stop if the person does guess right
 
-function count_files() {
 num_of_files=$((ls -A | wc -l))
-}
+integers='[^0-9]+$'
 
-function ask_for_input() {
+function game() {
 echo "Hello player! Let's test your guessing skills! How many files do you think are in the current directory?"
-read response
+read response 
 }
 
-function input_isnum() {
-integers='^[0-9]+$'
-if ! [[ $response=$integers ]] ; then
-  echo "I'm only good at reading integers. You understand, right? Please enter an integer as your guess."
-elif [[ $response=$integers.$integers ]] ; then
-  echo "Did you know that you can't have a fraction of a file in a directory? Wild! Please, enter a whole number this time."
-else 
-  $output=$good
-fi
-} 
-
-function main() {
-while [[ $output=$good ]] ; do
-  if [[ $response -lt $num_of_files ]] ; then
+for [[ response in $game() ]] ; do
+  if ! [[ $response=$integers ]] ; then
+    echo "I'm only good at reading integers. You understand, right? Please enter an integer as your guess."
+    read new_response ; new_response > response
+  elif [[ $response=$integers && $response -lt $num_of_files ]] ; then
     echo "You guessed too low! Try again, better luck next time!"
-  elif [[ $response -gt $num_of_files ]] ; then
+    read new_response ; new_response > response
+  elif [[ $response=$integers && $response -gt $num_of_files ]] ; then
     echo "You guessed too high! Try again, better luck next time!"
-  elif [[ $response -eq $num_of_files ]] ; then
+    read new_response ; new_response > response
+  elif [[ $response=$integers && $response -eq $num_of_files ]] ; then
     echo "How'd you guess? That's right! Let's play again some time."
+  else
+    echo "Hmm. Something's wrong with your input. Try again and make sure it's a positive, whole number."
+    read new_response ; new_response > response
   fi
 done
-}
+
